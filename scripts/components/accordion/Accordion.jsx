b6b63794879639acd6e941814d4dd1c8a2d2c6ae
Alexandru Simache <alex@Alexandrus-MacBook-Pro.local>
2022-04-06 12:12:45 +0200

    adding react js

---
+++ scripts/components/accordion/Accordion.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+import ReactDom from "react-dom"
+
+const Accordion = () => {
+  return <div>React JS Accordion</div>
+}
+
+const root = document.getElementById('react-accordion');
+ReactDom.render(<Accordion />, root);
